2b24cf99ce8c03cac7324f7afc1e6ff08ef25c99
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2026-03-20 22:00:13 +1100

    Fixes

--- basilisk.nvim/lua/basilisk/config.lua
+++ basilisk.nvim/lua/basilisk/config.lua
@@ -92,7 +92,7 @@ M.defaults = {
     executable_path = nil,
     auto_sync = false,
     stub_suggestions = true,
-    dependency_diagnostics = true,
+    dependency_diagnostics = false,
   },
   keymaps = {
     enabled = true,
